908cb42da403ed5c64a2799badcf2aa01e6ab834
r-bytes <101632683+r-bytes@users.noreply.github.com>
2022-10-21 01:06:31 +0200

    style: tailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,8 @@
 module.exports = {
     mode: 'jit',
     content: [
-      "./**/*.{js,ts,jsx,tsx}",
+      "./pages/*.{js,ts,jsx,tsx}",
+      "./components/*.{js,ts,jsx,tsx}",
     ],
     darkMode: "class",
     theme: {
