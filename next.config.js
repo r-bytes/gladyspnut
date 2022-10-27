8b81817038b5c673492b5a813b1b620d634808b0
r-bytes <101632683+r-bytes@users.noreply.github.com>
2022-10-27 17:32:05 +0200

    fix: add assets

--- next.config.js
+++ next.config.js
@@ -2,6 +2,22 @@
 const nextConfig = {
     reactStrictMode: true,
     swcMinify: true,
+    images: {
+        remotePatterns: [
+        {
+            protocol: 'https',
+            hostname: 'scontent-ams4-1.cdninstagram.com',
+            port: '',
+            pathname: '/v/t51.2885-15/**',
+        },
+        {
+            protocol: 'https',
+            hostname: 'scontent-ams2-1.cdninstagram.com',
+            port: '',
+            pathname: '/v/t51.2885-15/**',
+        },
+        ],
+    },
 };
 
 module.exports = nextConfig;
