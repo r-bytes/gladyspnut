3ca955ecf8b1c9940e8b6c0c7bf05de08cf68a38
r-bytes <101632683+r-bytes@users.noreply.github.com>
2022-10-27 23:42:28 +0200

    feat: add commission component

---
+++ components/Commission.jsx
@@ -0,0 +1,19 @@
+
+const Commission = ({ images, title, price, info }) => {
+
+    console.log()
+    return (
+        <div>
+            {/* <Image /> */}
+            <h1> Title </h1>
+            <h3> price </h3>
+            <ul>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+            </ul>
+        </div>
+    )
+}
+export default Commission
--- pages/commission-info.jsx
+++ pages/commission-info.jsx
@@ -1,6 +1,18 @@
-const CommissionInfo = () => {
-    console.log("CommissionInfo loaded =>");
+import { GridRegular } from "@components/index";
+import images from "@images/images.json"
 
-    return <div>CommissionInfo</div>;
+
+const Commission = () => {
+    const selectedImages = images.filter(i => i.page === "commissionInfo")
+
+    return (
+        <GridRegular
+            title={"Commission Info"}
+            subTitle={"I’m available for custom artwork, emotes, loyalty badges, animated gifs and branding."}
+            intro={`I listed a few popular commissions, but feel free to contact me for other ideas and projects.  \n`}
+            images={selectedImages}
+        />
+        
+    )
 };
-export default CommissionInfo;
+export default Commission;
