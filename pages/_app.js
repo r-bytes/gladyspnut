1d7ddbfb5a4b79ec71ea871889a70f9947560dd4
r-bytes <101632683+r-bytes@users.noreply.github.com>
2022-10-27 23:41:22 +0200

    feat: images

--- pages/_app.js
+++ pages/_app.js
@@ -9,7 +9,7 @@ function MyApp({ Component, pageProps }) {
             <ThemeProvider>
                 <Navbar />
                 <Component {...pageProps} />
-                <Footer />
+                <Footer  />
             </ThemeProvider>
         </>
     );
