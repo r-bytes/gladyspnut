bbc3a243b4dd1d5e2eb412c73c200e11a69aa650
r-bytes <101632683+r-bytes@users.noreply.github.com>
2022-10-28 22:54:18 +0200

    feat: commission component

--- components/Commission.jsx
+++ components/Commission.jsx
@@ -1,18 +1,30 @@
+import { Carousel } from "."
+import images from "@images/images.json"
+import Image from "next/image"
 
-const Commission = ({ images, title, price, info }) => {
 
-    console.log()
+const Commission = () => {
+    const selectedCarouselImages = images.find(i => i.page === "commissionInfo" && i.carousel)
+    const selectedImages = images.filter(i => i.page === "commissionInfo" && !i.carousel)
+
     return (
-        <div>
-            {/* <Image /> */}
-            <h1> Title </h1>
-            <h3> price </h3>
-            <ul>
-                <li></li>
-                <li></li>
-                <li></li>
-                <li></li>
-            </ul>
+        <div className="ml-16">
+            {selectedImages.map(img => (
+                <div className="w-full flex-shrink-0 left-0 right-0" key={img.id}>
+                    <Image src={img.imageUrl} className="w-full object-contain" width={250} height={250} alt="#" />
+                </div>
+            ))}
+
+            {/* carousel */}
+            {selectedCarouselImages.length ? selectedCarouselImages.map(imgs => (
+                <div className="w-full flex-shrink-0 left-0 right-0" key={imgs.id}>
+                    <Carousel key={imgs.id} images={imgs} />
+                </div>
+            )) :
+                <div className="w-full flex-shrink-0 left-0 right-0">
+                    <Carousel images={selectedCarouselImages} />
+                </div>
+            }
         </div>
     )
 }
