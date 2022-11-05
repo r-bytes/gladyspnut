9cddce343e5ebeb66af98d6eb2c4fcac203478b9
r-bytes <101632683+r-bytes@users.noreply.github.com>
2022-11-05 01:46:38 +0100

    refactor: cleanup

--- components/index.js
+++ components/index.js
@@ -1,4 +1,3 @@
-export { default as Header } from "./Header.jsx";
 export { default as Footer } from "./Footer.jsx";
 export { default as Theme } from "./Theme.jsx";
 export { default as Menu } from "./MenuListComposition.jsx";
