06cfaad42918aa4e8e5fa18a06e77c114e26f3d9
r-bytes <101632683+r-bytes@users.noreply.github.com>
2022-11-06 18:05:58 +0100

    feat: scroll event

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -1,6 +1,7 @@
 import { IconButton } from "@mui/material";
 import Image from "next/image"
 import React, { useState } from "react"
+import { Parallax, Background } from 'react-parallax';
 
 const Hero = ({ heading, message, testRef }) => {
     const scrollToElement = () => testRef.current.scrollIntoView({ behavior: 'smooth', duration: 3000, block: "start" });
@@ -19,7 +20,7 @@ const Hero = ({ heading, message, testRef }) => {
     
     return (
         <div className="flex flex-col items-center justify-between h-screen">
-            <div className="flex flex-col items-center mt-72">
+            <div className="flex flex-col items-center mt-72 p-24 rounded-lg bg-card backdrop-blur-xl shadow-2xl">
                 <h2 className="text-5xl font-bold tracking-wider"> {heading} </h2>
                 <p className="py-5 text-xl text-center leading-loose tracking-wider">
                     {message.split("\n").map((line, index) => (
@@ -29,15 +30,12 @@ const Hero = ({ heading, message, testRef }) => {
                         </React.Fragment>
                     ))} 
                 </p>
-                <button className="uppercase font-bold tracking-widest py-2 px-8 border hover:bg-button hover:text-secondary hover:border-none"> book </button>
+                <button className="rounded uppercase font-bold tracking-widest py-2 px-8 border hover:bg-button hover:text-secondary hover:border-none"> book </button>
             </div>
-            {scroll ? (
-                ""
-            ) : (
-                <IconButton className="flex items-center justify-center" onClick={scrollToElement}>
+            {scroll ? ("") : (
+                <IconButton className="flex items-center justify-center mb-8" onClick={scrollToElement}>
                     <Image alt="arrowDown" src={"/assets/down-arrow.svg"} height={30} width={30} className="animate-bounce cursor-pointer"  />
                 </IconButton>
-
             )}
         </div>
     )
