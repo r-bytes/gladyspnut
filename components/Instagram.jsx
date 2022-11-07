6aeb4fbd1e08d3ec356ba9268b50bdc9eed8c5aa
r-bytes <101632683+r-bytes@users.noreply.github.com>
2022-11-07 12:22:30 +0100

    style: add hovers

--- components/Instagram.jsx
+++ components/Instagram.jsx
@@ -20,30 +20,26 @@ const Instagram = () => {
     const carouselImages = posts?.filter(p => p.media_type === "CAROUSEL_ALBUM")
 
     return (
-        <div className="max-w-4xl md:w-5/6
-             mx-auto  bg-card rounded-xl my-32 
-            backdrop-blur-3xl shadow-2xl px-12
-            grid place-items-center sm:place-items-start grid-cols-1
-            sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-4">
-            <h1 className="col-span-full text-5xl font-bold mb-16 mt-12"> {"Instagram Feed"} </h1>
+        <div className="max-w-4xl md:w-5/6 mx-auto  bg-card rounded-xl my-32 backdrop-blur-3xl shadow-2xl px-12 grid place-items-center sm:place-items-start grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-4">
+            <h1 className="col-span-full text-5xl font-bold mb-16 mt-16"> Instagram Feed </h1>
                 {images?.map(i => (
-                    <a className="mx-auto" key={i.id} href={i.permalink}>
+                    <a className="mx-auto hover:scale-105" key={i.id} href={i.permalink}>
                         <Image className="rounded-2xl" src={i.media_url} height={300} width={300} alt="2" />
                     </a>
                 ))}
                 {carouselImages?.map(i => (
-                    <a className="mx-auto" key={i.id} href={i.permalink}>
+                    <a className="mx-auto hover:scale-105" key={i.id} href={i.permalink}>
                         <Image className="rounded-2xl" src={i.media_url} height={300} width={300} alt="2" />
                     </a>
                 ))}
                 <div className="col-span-full my-12 mb-24 mx-auto">
                     <a href={`https://www.instagram.com/raysolles`} target="_blank" rel="noReferrer">
-                        <span
-                            className="rounded-2xl shadow-2xl hover:underline hover:translate-y-1" style={{fontSize: "18px", padding: ".8rem 1.7rem", color: "#ffffff",
+                        <button
+                            className="hover:scale-105 rounded-full shadow-2xl hover:translate-y-1" style={{fontSize: "18px", padding: ".8rem 1.7rem", color: "#ffffff",
                                 backgroundImage: "linear-gradient(to right,#405de6,#5851db, #833ab4, #c13584, #e1306c, #fd1d1d)"
                             }}>
                             Follow @ raysolles
-                        </span>
+                        </button>
                     </a>
                 </div>
         </div>
