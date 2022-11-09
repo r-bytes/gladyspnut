1d232c808f388441c906deed2a577817217e3e33
r-bytes <101632683+r-bytes@users.noreply.github.com>
2022-11-09 01:42:15 +0100

    fix: fix

--- components/GridImage.jsx
+++ components/GridImage.jsx
@@ -2,7 +2,7 @@ import { GridImageItem } from ".";
 
 const GridImage = ({ filteredImages }) => {
 
-    const cardImages = filteredImages.map(({_id, title, stackInfo, mainImage }) => (
+    const cardImages = filteredImages?.map(({_id, title, stackInfo, mainImage }) => (
         <GridImageItem
             key={_id}
             _id={_id}
@@ -14,7 +14,7 @@ const GridImage = ({ filteredImages }) => {
         
     return (
         <div className="py-56 min-h-fit">
-            <h1 className="text-5xl font-bold mb-16 mt-12 text-center"> {filteredImages[0].pageInfo.displayname} </h1>
+            <h1 className="text-5xl font-bold mb-16 mt-12 text-center"> {filteredImages[0]?.pageInfo.displayname} </h1>
             <div className="max-w-5xl bg-card shadow-2xl backdrop-blur-2xl rounded= sm:p-12 mx-auto grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8">
                 {cardImages}
             </div>
