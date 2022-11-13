aa615f911c1b6f091285faa22ddb8493f9ef75c0
r-bytes <101632683+r-bytes@users.noreply.github.com>
2022-11-13 17:52:33 +0100

    refactor: insta update

--- components/Instagram.jsx
+++ components/Instagram.jsx
@@ -8,32 +8,34 @@ const Instagram = () => {
         .then(response => {
             return response.json()
         })
-        .then(result => setPosts(result.data))
+        .then(result => {
+            setPosts(result.data)
+        })
     }, [])
 
     const images = posts?.filter(p => p.media_type === "IMAGE")
     const carouselImages = posts?.filter(p => p.media_type === "CAROUSEL_ALBUM")
 
     return (
-        <div className="md:max-w-4xl lg:max-w-5xl xl:max-w-7xl max-w-7xl mx-auto bg-card rounded my-32 backdrop-blur-3xl shadow-2xl px-12 grid place-items-center sm:place-items-start grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-4">
+        <div className="md:max-w-4xl lg:max-w-5xl xl:max-w-7xl max-w-7xl mx-4 lg:mx-auto bg-secondary rounded my-32 backdrop-blur-3xl shadow-2xl px-12 grid place-items-center sm:place-items-start grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-4">
             <h1 className="col-span-full text-5xl font-bold mb-16 mt-16"> Instagram Feed </h1>
                 {images?.map(i => (
-                    <a className="mx-auto hover:scale-105" key={i.id} href={i.permalink}>
+                    <a className="mx-auto hover:scale-105" key={i.id} href={i.permalink} target="_blank" rel="noReferrer">
                         <Image className="rounded" src={i.media_url} height={300} width={300} alt="2" />
                     </a>
                 ))}
                 {carouselImages?.map(i => (
-                    <a className="mx-auto hover:scale-105" key={i.id} href={i.permalink}>
+                    <a className="mx-auto hover:scale-105" key={i.id} href={i.permalink} target="_blank" rel="noReferrer">
                         <Image className="rounded" src={i.media_url} height={300} width={300} alt="2" />
                     </a>
                 ))}
                 <div className="col-span-full my-12 mb-24 mx-auto">
-                    <a href={`https://www.instagram.com/raysolles`} target="_blank" rel="noReferrer">
+                    <a href={`https://www.instagram.com/gladyspnut`} target="_blank" rel="noReferrer">
                         <button
                             className="hover:scale-105 rounded-full shadow-2xl hover:translate-y-1" style={{fontSize: "18px", padding: ".8rem 1.7rem", color: "#ffffff",
                                 backgroundImage: "linear-gradient(to right,#405de6,#5851db, #833ab4, #c13584, #e1306c, #fd1d1d)"
                             }}>
-                            Follow @ raysolles
+                            Follow @ gladyspnut
                         </button>
                     </a>
                 </div>
